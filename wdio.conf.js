577e76837aab4722e62ec6823ed1d3e34eb958c0
Masakhane Matanzima <sihle.matanzima@gmail.com>
2020-11-10 22:25:47 +0200

    allure report

--- wdio.conf.js
+++ wdio.conf.js
@@ -128,7 +128,11 @@ exports.config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter.html
-    reporters: ['spec'],
+    reporters: [['allure',{
+        outputDir:'allure-results',
+        disabledWebdriverStepReporting:true,
+        disabledWebdriverScreenshotReporting:false,
+    }]],
 
 
     
